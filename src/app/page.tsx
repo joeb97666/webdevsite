695827e1a8d8a32314eac3410e3be51e449a9bae
joeb97666 <joeb97666@gmail.com>
2024-01-04 12:09:53 -0800

    recommit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@ import Image from 'next/image'
 import { V0Navigatior } from '@/components/v0-navigator'
 export default function Home() {
   return (
-<div>
-    <V0Navigatior/>
+    <><div>
+      <V0Navigatior />
 
-    <h1>Home Page</h1>
-</div>
+
+    </div>
+    <div></div></>
   )
 }
